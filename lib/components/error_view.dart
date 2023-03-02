05b7ebaabaa0a98bc9c75714200f9e23709e083a
shirobutton <shirobutton@gmail.com>
2023-03-03 04:31:51 +0900

    UIStack周りのViewを追加

---
+++ lib/components/error_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ErrorView extends StatelessWidget {
+  const ErrorView({
+    required this.text,
+    this.retry,
+    this.enableRetryButton = true,
+    super.key,
+  });
+
+  final String text;
+  final void Function()? retry;
+  final bool enableRetryButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(text),
+          if (enableRetryButton && retry != null)
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: ElevatedButton(
+                onPressed: retry,
+                child: const Text("リトライ"),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
